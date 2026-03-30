650a13848df2d7b2adf02e00667bdf7a39c0ef84
parth-pawarr <parthpawar2406@gmail.com>
2026-03-30 19:10:40 +0530

    Updated Terminal.jsx: changed size of Check icon from 20 to 15

--- src/windows/Terminal.jsx
+++ src/windows/Terminal.jsx
@@ -39,7 +39,7 @@ const Terminal = () => {
             </ul>
             <div className="footnote">
                 <p>
-                    <Check size={20} /> 5 out 5 stacks loaded successfully (100%)
+                    <Check size={15} /> 5 out 5 stacks loaded successfully (100%)
                 </p>
                 <p className="text-black">
                     <Flag size={20} fill="black"/>
